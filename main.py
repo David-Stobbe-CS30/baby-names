9c50ba10b3f7d803daa33485fa5534677f9d803b
David Stobbe <d.stobbe1@share.epsb.ca>
2023-09-26 13:18:12 -0600

    done

--- main.py
+++ main.py
@@ -2,11 +2,10 @@
 
 import json
 
-
 def main():
     # Load Baby Data from File
     file = open("baby-names-data.json")
-    baby_data = json.load(file)
+    baby_data = json.loads(file.read())
     file.close()
 
     # Main Menu
@@ -29,6 +28,7 @@ def main():
             loop = False
 
 
+
 def getMenuSelection():
     # Print Menu & Return User Selection
     print("\n*** BABY DATA - MAIN MENU ***")
@@ -41,31 +41,57 @@ def getMenuSelection():
 
     return input("* Enter Option #: ")
 
+def printList(babies):
+    for i in range(len(babies)):
+        print(f"{babies[i]['name']} (Rank: {babies[i]['rank']}, Gender: {babies[i]['gender']})")
 
 def displayAll(baby_data):
     # Display All Baby Data
     print("\nDISPLAY ALL")
+    printList(baby_data)
 
 
 def searchGender(baby_data):
     # Dislay All Baby Names based on Gender
     print("\nSEARCH BY GENDER")
+    gender = input("Enter a gender (Boy/Girl): ")
+    babies = []
+    for i in range(len(baby_data)):
+        if(baby_data[i]['gender'] == gender):
+            babies.append(baby_data[i])
+    printList(babies)
 
 
 def searchRank(baby_data):
     # Display All Baby Names based on Rank
-    print("\nSEARCH BY RANK")
-
+    print("\nSEARCH BY RANK")    
+    rank = int(input("Enter a rank: "))
+    babies = []
+    for i in range(len(baby_data)):
+        if(baby_data[i]['rank'] == rank):
+            babies.append(baby_data[i])
+    printList(babies)
 
 def searchStartLetter(baby_data):
     # Insert User Item into a Position
     print("\nSEARCH BY START LETTER")
+    letter = input("Enter a letter: ")
+    babies = []
+    for i in range(len(baby_data)):
+        if(baby_data[i]['name'][0] == letter):
+            babies.append(baby_data[i])
+    printList(babies)
 
 
 def searchNameLength(baby_data):
     # Remove item from position
     print("\nSEARCH BY NAME LENGTH")
-
+    length = int(input("Enter length: "))
+    babies = []
+    for i in range(len(baby_data)):
+        if(len(baby_data[i]['name']) == length):
+            babies.append(baby_data[i])
+    printList(babies)
 
 # Invoke main to begin program
 main()
